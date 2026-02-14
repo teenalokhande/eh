d023a9ea55dee915aa419dd5a5cc857d0a7412af
teenalokhande <lokhandet87@gmail.com>
2026-02-14 10:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,5 +29,8 @@ def main():
 
     print("\nanalysis completed. report generated.")
 
+
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
